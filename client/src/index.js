0c20e66dd5b79d1bd423d56792f1d0255d42a908
prasanthrv98 <prasanthoffcl@gmail.com>
2023-07-11 13:38:45 +0530

    Initial setup

---
+++ client/src/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import App from "./App"
+import { configureStore } from "@reduxjs/toolkit"
+import { Provider } from "react-redux"
+import globalReduccer from "state"
+
+const store = configureStore({
+  reducer: {
+    global: globalReduccer
+  }
+})
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+)
---
+++ server/index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import helmet from "helmet";
+import mongoose from "mongoose";
+
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
+
+// CONFIGURATION
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// Routes
+app.use("/client", clientRoutes);
+app.use("/general", generalRoutes);
+app.use("/management", managementRoutes);
+app.use("/sales", salesRoutes);
+
+// Mongoose setup
+const PORT = process.env.PORT || 9000;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewURLParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server Port: ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`Not Connected ${err}`);
+  });
